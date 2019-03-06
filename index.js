8c74e1961567d4b20a135aa8a0ba830e9e004196
Olda Salek <mzk@mozektevidi.net>
2019-03-06 13:46:51 +0100

    persist messages

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+var messages = []
+
 io.on('connection', function (socket) {
   console.log('a user connected')
   console.log('socket', socket)
   io.emit('this', { will: 'be received by everyone' })
 
-  io.emit(socket.client.id).emit('chat message', {message: socket.client.id + ' just joined', nick: 'admin'})
+  messages.forEach((item) => {
+    io.to(socket.client.id).emit('chat message', item)
+  })
+  io.emit(socket.client.id).emit('chat message', { message: socket.client.id + ' just joined', nick: 'admin' })
   socket.on('chat message', function (msg) {
+    messages.push(msg)
     console.log('msg', msg)
     io.emit('chat message', msg)
   })
