43dbff52b3df82a944e03bdcf69ef45222bd273f
Olda Salek <mzk@mozektevidi.net>
2019-03-06 13:46:28 +0100

    reformat, info messages

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  console.log('socket', socket)
+  io.emit('this', { will: 'be received by everyone' })
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+  io.emit(socket.client.id).emit('chat message', {message: socket.client.id + ' just joined', nick: 'admin'})
+  socket.on('chat message', function (msg) {
+    console.log('msg', msg)
+    io.emit('chat message', msg)
+  })
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+
+  socket.emit('news', { hello: 'world' })
+
+})
+
+http.listen(port, function () {
+  console.log('listening on *:' + port)
+})
